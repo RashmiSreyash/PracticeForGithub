436e17dfd0bdb74afe14443d0e633dcf62d954fe
RASHMI <RASHMI@LAPTOP-AHA78SJE>
2022-01-20 20:12:02 +0530

    First Push

---
+++ PracticeGithub/src/test/java/practiceForGithub/Demo1.java
@@ -0,0 +1,14 @@
+package practiceForGithub;
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void democlass1()
+	{
+		int value=2345;
+		System.out.println("This is demo class for github");
+		System.out.println(value);
+	}
+
+}
---
+++ PracticeGithub/src/test/java/practiceForGithub/Demo2.java
@@ -0,0 +1,15 @@
+package practiceForGithub;
+
+import org.testng.annotations.Test;
+
+public class Demo2 
+{
+	@Test
+	public void democlass2()
+	{
+		System.out.println("this is 2nd demo class");
+		System.out.println("hello");
+		System.out.println("bye");
+	}
+
+}
